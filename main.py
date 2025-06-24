7be9863413ab1ba8b9079b6d7246fcd4fde8c0ce
poloniumOP <shreesha8406@gmail.com>
2025-06-24 18:12:02 +0530

    update1, added more roles

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import logging
+# import logging # not being used currently
 from dotenv import load_dotenv
 import os
 import webserver
@@ -17,10 +17,12 @@ intents.auto_moderation = True
 bot = commands.Bot(command_prefix='?', intents=intents)
 # global msgid
 msgid = 1380881692557836329
+rldct = {'1️⃣': "Batch of 2027'",'2️⃣': "Batch of 2028'",'3️⃣': "Batch of 2029'", '0️⃣': "Batch of 2026'", '⭐': "Graduated (Ex-RVian)"}
+
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} active!')
+    print(f'{bot.user.name} active')
 
 
 # @bot.event
@@ -38,6 +40,16 @@ async def rol(ctx):
     await rmsg.add_reaction('2️⃣')
     await rmsg.add_reaction('3️⃣')
 
+@bot.command()
+async def upd1(ctx):
+    ch = ctx.channel
+    m = await ch.fetch_message(msgid)
+    newemb = discord.Embed(title="1. Batch of 2027\n2. Batch of 2028\n3. Batch of 2029\n0. Batch of 2026\n⭐. Graduated", color=0x00ff00)
+
+    await m.edit(embed=newemb)
+    await m.add_reaction('0️⃣')
+    await m.add_reaction('⭐')
+
 @bot.event
 async def on_raw_reaction_add(rxn):
     if rxn.message_id != msgid:
@@ -45,8 +57,9 @@ async def on_raw_reaction_add(rxn):
 
     gld = bot.get_guild(rxn.guild_id)
     u = gld.get_member(rxn.user_id)
+    if u is None or u.bot:
+        return
 
-    rldct = {'1️⃣': "Batch of 2027'",'2️⃣': "Batch of 2028'",'3️⃣': "Batch of 2029'"}
     # while editing make sure that the role names end with '
     btc = rldct.get(rxn.emoji.name)
     for rl in rldct.values():
@@ -65,8 +78,9 @@ async def on_raw_reaction_remove(rxn):
 
     gld = bot.get_guild(rxn.guild_id)
     u = gld.get_member(rxn.user_id)
+    if u is None or u.bot:
+        return
 
-    rldct = {'1️⃣': "Batch of 2027'",'2️⃣': "Batch of 2028'",'3️⃣': "Batch of 2029'"}
     # while editing make sure that the role names end with '
     btc = rldct.get(rxn.emoji.name)
     if btc is not None:
